8a7e046adf94e4814f97c0dcca7048dcb5378d8d
Hello-Vince <yin415+github@gmail.com>
2024-02-06 11:21:40 -0500

    first GET endpoint

--- src/routes/status.rs
+++ src/routes/status.rs
@@ -6,13 +6,22 @@ use tracing::debug;
 use crate::errors::Error;
 
 pub fn create_route() -> Router {
-  Router::new().route("/status", get(get_status))
+  Router::new()
+    .route("/status", get(get_status))
+    .route("/", get(get_greeting))
 }
 
 async fn get_status() -> Result<Json<Status>, Error> {
   debug!("Returning status");
   Ok(Json(Status {
     status: "ok".to_owned(),
+  }))
+}
+
+async fn get_greeting() -> Result<Json<Status>, Error> {
+  debug!("Returning status");
+  Ok(Json(Status {
+    status: "Welcome to the RustAPI!".to_owned(),
   }))
 }
 
